ecfc6ff8b2b1c2c8f437787900c015d314e41394
Bill Bourne <bill@bashd.ca>
2019-10-31 21:56:07 -0400

    updating with solutions to homework 2

---
+++ hw2/hw2cmd/hw2main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"p4l/hw2"
+)
+
+func main() {
+	fmt.Println("Samples for Stepik String Functions Assignment ")
+
+	fmt.Println(hw2.ClumpFinding("ACGTACGT", 1, 5, 2))
+
+}
